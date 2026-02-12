4e89c44b8e89cccba98b026721503da322a89f71
darken <darken@darken.eu>
2026-02-12 15:57:26 +0100

    refactor: use shared clientIp() in MyIpRoute instead of inline logic
    Removes duplicated IP extraction logic that had diverged from the
    shared implementation in IpHelper.kt.

--- src/main/kotlin/eu/darken/octi/kserver/myip/MyIpRoute.kt
+++ src/main/kotlin/eu/darken/octi/kserver/myip/MyIpRoute.kt
@@ -1,8 +1,7 @@
 package eu.darken.octi.kserver.myip
 
-import eu.darken.octi.kserver.common.IpHelper
+import eu.darken.octi.kserver.common.clientIp
 import io.ktor.http.*
-import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import javax.inject.Inject
@@ -13,18 +12,7 @@ class MyIpRoute @Inject constructor() {
 
     fun setup(rootRoute: Routing) {
         rootRoute.get("/v1/myip") {
-            val connectionIp = call.request.origin.remoteAddress
-            val clientIp = if (IpHelper.isLoopback(connectionIp)) {
-                // Request is from a local reverse proxy, trust X-Forwarded-For
-                call.request.headers["X-Forwarded-For"]
-                    ?.split(",")
-                    ?.firstOrNull()
-                    ?.trim()
-                    ?.takeIf { IpHelper.isValid(it) }
-                    ?: connectionIp
-            } else {
-                connectionIp
-            }
+            val clientIp = call.request.clientIp()
             call.respond(HttpStatusCode.OK, mapOf("ip" to clientIp))
         }
     }
